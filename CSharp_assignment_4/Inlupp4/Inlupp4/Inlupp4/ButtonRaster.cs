2f380faac573a5b499055e4e539f73fdd97704ab
Ferhat Sevim <86186746+ofk16fsm@users.noreply.github.com>
2021-06-20 17:06:26 +0200

    Add files via upload

---
+++ CSharp_assignment_4/Inlupp4/Inlupp4/Inlupp4/ButtonRaster.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Inlupp4
+{
+    public class ButtonRaster : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public ButtonRaster()
+        {
+        }
+
+        protected override void OnClick()
+        {
+            //
+            //  TODO: Sample code showing how to access button host
+            //
+           
+            ArcMap.Application.CurrentTool = null;
+            View msgView = new View();
+            msgView.Show();
+        }
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;
+        }
+    }
+
+}
