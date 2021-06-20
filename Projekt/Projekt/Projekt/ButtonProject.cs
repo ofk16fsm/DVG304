5a980079fcb5e7d1cf6c2efcbaf0fef65d064b2a
Ferhat Sevim <86186746+ofk16fsm@users.noreply.github.com>
2021-06-20 16:33:06 +0200

    Add files via upload

---
+++ Projekt/Projekt/Projekt/ButtonProject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Projekt
+{
+    public class ButtonProject : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public ButtonProject()
+        {
+        }
+
+        protected override void OnClick()
+        {
+            //
+            //  TODO: Sample code showing how to access button host
+            //
+            View msgView = new View();
+            msgView.Show();
+            ArcMap.Application.CurrentTool = null;
+        }
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;
+        }
+    }
+
+}
